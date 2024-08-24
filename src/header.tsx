580bc1ab8714814508e7ef0d831c15a0cbf2bb49
AyushMaharwade <ayushmaharwade58@gmail.com>
2024-08-24 16:53:26 +0530

    new file:   Assets/image.png 	new file:   Assets/png-clipart-three-headed-lion-lion-capital-of-ashoka-sarnath-museum-state-emblem-of-india-national-symbols-of-india-planet-miscellaneous-mammal 1.png 	modified:   src/header.tsx 	modified:   src/poster.jsx

--- src/header.tsx
+++ src/header.tsx
@@ -3,27 +3,32 @@ import React from "react"
 const Header=()=>{
     return (
         <>
-    <nav className="px-4 lg:px-6 lg:py-0 py-2.5 lg:h-[9vh] border-b-2 border-[#BEADAD] ">
+    <nav className="px-4 py-3 lg:px-6 lg:py-0 lg:h-[9vh] bg-[#FB8500]">
         <div className="flex flex-wrap lg:justify-between
         items-center mx-auto max-w-screen-xl font-Inter justify-center gap-3 lg:h-[9vh]">
             <a href="#" className="flex items-center">
+                <img className="h-[50px] p-1 " src="Assets/png-clipart-three-headed-lion-lion-capital-of-ashoka-sarnath-museum-state-emblem-of-india-national-symbols-of-india-planet-miscellaneous-mammal 1.png" />
+
                 <span className="self-center flex justify-center items-center
-                whitespace-nowrap font-Inter font-medium text-3xl text-black">
+                whitespace-nowrap font-sans font-medium text-3xl text-white">
                     Constitution of India</span>
             </a>
-            <div className="flex items-center justify-evenly lg:order-2 w-[320px]">
-                <button className="text-white bg-[#FFBE0B] hover:bg-[#FB5607]
-                dark:text-white focus:bg-[#FB5607] 
-                rounded-[12px] text-md text-center lg:px-5 py-2 lg:py-2 mr-2 focus:outline-none font-Inter px-4 font-medium">Sign up</button>
-                
-                <div className="h-10 w-[1.8px] bg-[#BEADAD] "></div>
 
-                <button className="text-black bg-white 
-                hover:text-[#FF006E] focus:text-[#FF006E] 
-                 rounded-lg text-md py-2 lg:py-2
-                ml-2 dark:bg-primary-600 
-                dark:hover:bg-primary-700 focus:outline-none 
-                dark:focus:ring-primary-800 font-Inter font-semibold">Login</button>
+            <div>
+                <ul className="md:flex justify-between align-items lg:w-[30vw] md:w-[30vw] sm:w-[90vw] gap-[5px] text-white font-regular hidden">
+                    <li className="cursor-pointer">Home</li>
+                    <li className="cursor-pointer">About us</li>
+                    <li className="cursor-pointer">Learn</li>
+                    <li className="cursor-pointer">Games</li>
+                    <li className="cursor-pointer">Langauge</li>
+                </ul>
+            </div>
+
+            <div className="flex items-center justify-evenly lg:order-2 w-[320px]">
+                <button className="text-white bg-[#000C73] hover:bg-[#3e75c8]
+                dark:text-white focus:bg-[#FB5607] 
+                rounded-[12px] text-md text-center lg:px-5 py-2 lg:py-2 mr-2 focus:outline-none font-Inter px-4 font-medium">Login / Sign up</button>
+
             </div>       
         </div>
     </nav>
--- src/poster.jsx
+++ src/poster.jsx
@@ -3,8 +3,22 @@ import React from "react"
 const Poster=()=>{
     return(
         <>
-        <h1>Hello World</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, impedit?</p>
+        <div>
+            <img className="md:h-[60vh] md:w-[100vw] h-[40vh] absolute z-[-1]" src="Assets/image.png" />
+            <div className="md:h-[60vh] md:w-[100vw] h-[40vh] absolute z-[-1]  bg-gradient-to-t from-black to-transparent "></div>
+            <div className="w-auto md:h-[40vh] flex flex-col justify-around items-center align- z-10">
+                <h1 className="md:text-5xl text-3xl font-medium text-white text-center font-sans">Sansthaein Aur Samvidhan Institutions & Constitution</h1>
+                <p className="md:w-[70vw] text-white font-medium md:inline hidden">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora eos suscipit repudiandae quis reiciendis distinctio laborum aliquam. Cumque possimus, ratione ipsam voluptas laudantium, maiores odio corporis, hic ullam vitae quae reiciendis animi. Corporis sunt aliquid quaerat facere dolores voluptatem amet facilis! Voluptas voluptatibus incidunt, reprehenderit illo distinctio accusantium tempora corrupti?</p>
+            </div>
+            <div className="h-[30vh] w-auto flex justify-center items-center">
+
+            <div className="h-[30vh] w-[50vw] bg-gradient-to-r from-[#FB8500] to-[#FB8500]  text-white p-8">
+                <div className="mb-6 mt-1 text-3xl font-medium font-sans">About us</div>
+                <p className="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde optio vitae dicta. Eveniet accusantium, doloribus deleniti repellendus explicabo in officia fuga itaque corporis dolore quos soluta laudantium tempora temporibus maxime doloribus deleniti repellendus explicabo in officia fuga itaque corporis dolore quos soluta laudantium tempora temporibus maxime .</p>
+            </div>
+
+            </div>
+        </div>
         </>
     )
 }
