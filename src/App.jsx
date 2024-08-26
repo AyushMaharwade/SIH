ce6646ebdffd06c758e394dbcf759356ac60f383
Rakesh Sinha <112483526+rakeshsinha-14@users.noreply.github.com>
2024-08-26 14:16:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+// header
 import Header from './header'
+
+
 import Poster from './poster'
 import ForUser from './foruser'
-import Footer from './footer'
+
+
+
+// login page
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ForgetPasswordPage from './pages/ForgetPasswordPage';
+import HomePage from './pages/HomePage';
+
+// footer
+import Footer from './components/foot/footer'
 
 
 function App() {
 
   return (
     <>
-      <Header></Header>
-      <Poster></Poster>
-      <ForUser></ForUser>
-      <Footer></Footer>
+      <Router>
+        <div>
+                <Routes>
+                    <Route path="/" element={<LoginPage />} /> {/* Login page as the landing page */}
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/forget-password" element={<ForgetPasswordPage />} />
+                    <Route path="/home" element={<HomePage />} />
+                  <Routes/>
+          </div>
+      
+      <Footer/>
+      <Router/>
     </>
   )
 }
