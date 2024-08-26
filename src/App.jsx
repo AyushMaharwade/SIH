79e02bc144ca212fffdf30ce3d753742fe087550
Rakesh Sinha <112483526+rakeshsinha-14@users.noreply.github.com>
2024-08-26 12:43:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,17 @@ import './App.css'
 import Header from './header'
 import Poster from './poster'
 import ForUser from './foruser'
-import Footer from './footer'
+import Footer from './components/foot/footer'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
       <Header></Header>
       <Poster></Poster>
       <ForUser></ForUser>
-      <Footer></Footer>
+      <Footer/>
       
     </>
   )
