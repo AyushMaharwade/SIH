b5bee7bb72bc070632a5a55aae34467ccb02c9b6
Rakesh Sinha <112483526+rakeshsinha-14@users.noreply.github.com>
2024-08-26 13:53:38 +0530

    Add files via upload

---
+++ src/components/foot/FooterButtons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FooterButtons() {
+  return (
+    <div className="footer-buttons">
+      <button className="footer-button">About Us</button>
+      <button className="footer-button">Feedback</button>
+      <button className="footer-button">Feedback</button>
+    </div>
+  );
+}
+
+export default FooterButtons;
---
+++ src/components/foot/FooterLogo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FooterLogo() {
+  return (
+    <img 
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Emblem_of_India.svg/564px-Emblem_of_India.svg.png" 
+      alt="Logo" 
+      className="footer-logo-img"
+    />
+  );
+}
+
+export default FooterLogo;
